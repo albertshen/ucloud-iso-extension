a041fe96b346a2b84c5f1fc6a563f30222e7a07d
Albert Shen <albertshen1206@gmail.com>
2017-03-07 18:35:22 +0800

    I

---
+++ deploy/petitbateau-launch.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-petitbateau-launch
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.MD \
+        --exclude=Vagrantfile \
+        --exclude=config/config.php \
+        . /data/sites/default/
+
+echo "1q2w3e" | sudo -S service php7.0-fpm restart
