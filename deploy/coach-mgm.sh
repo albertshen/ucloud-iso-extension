a34f8c81cba7c2e71a1360b40c63e5ce0070460c
Albert Shen <albertshen1206@gmail.com>
2017-03-13 12:21:20 +0800

    i

---
+++ deploy/coach-mgm.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-coach-mgm
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=dist/asset/media \
+        --exclude=Vagrantfile \
+        --exclude=config/config.php \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        --exclude=config.rb \
+        --exclude=*.sql \
+        . /data/sites/default/
