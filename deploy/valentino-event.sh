47beeba108649d500f9cf4798687a8976e78192c
Albert Shen <albertshen1206@gmail.com>
2017-04-26 14:45:05 +0800

    i

---
+++ deploy/valentino-event.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-valentino-event
+
+git checkout master
+
+git pull
+
+cd /data/repository/sha-valentino-event/event
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=Vagrantfile \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        . /data/sites/default/event
+
+cd /data/repository/sha-valentino-event/ppc
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=Vagrantfile \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        . /data/sites/default/ppc
---
+++ deploy/valentino-pay.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-valentino-pay
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=Vagrantfile \
+        --exclude=config/config.php \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        --exclude=config.rb \
+        --exclude=upload \
+        --exclude=event \
+        --exclude=ppc \
+        --exclude=*.sql \
+        . /data/sites/default/
