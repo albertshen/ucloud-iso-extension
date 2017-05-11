1a0fdbca0ca9a0f312decafbb9c7c7cb446f73da
Albert Shen <albertshen1206@gmail.com>
2017-05-11 16:16:11 +0800

    i

---
+++ deploy/lv-series2.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+set -x
+set -e
+
+cd ~/repository/sha-lv-series2
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=.vagrant \
+        --exclude=README.md \
+        --exclude=phpmyadmin/ \
+        --exclude=ws/upload \
+        --exclude=media/ \
+        --exclude=ws/private/conf/config.ini.php \
+        . /data/sites/lvseries2/
