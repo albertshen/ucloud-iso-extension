7ee6b61d6ada7117487222d2d835831d6103b367
Albert Shen <albertshen1206@gmail.com>
2017-06-05 12:05:34 +0800

    i

---
+++ deploy/coach-2017-mothersday.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-coach-mothersday2017
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=Vagrantfile \
+        --exclude=config/config.php \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        --exclude=config.rb \
+        --exclude=build/dist/media \
+        --exclude=upload \
+        --exclude=*.sql \
+        . /data/sites/default/
