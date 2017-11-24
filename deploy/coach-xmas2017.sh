f136913a7508dbf7c8c2a207ec97e7e6c5ac1764
Albert Shen <albertshen1206@gmail.com>
2017-11-24 14:39:51 +0800

    i

---
+++ deploy/coach-xmas2017.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+set -e
+
+cd /data/repository/sha-coach-xmas2017/
+
+git checkout master
+
+git pull
+
+rsync -arvP --delete \
+        --exclude=.git \
+        --exclude=.gitignore \
+        --exclude=README.md \
+        --exclude=Vagrantfile \
+        --exclude=puppet \
+        --exclude=config/config.php \
+        --exclude=gulpfile.js \
+        --exclude=package.json \
+        --exclude=config.rb \
+        --exclude=db \
+        . /data/sites/default/
